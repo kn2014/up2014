2787be6568723743152e040d9ea9c03baa88f91f
kn2014 <cs1.8@mail.bg>
2014-11-06 15:19:44 +0200

    Update 1.cpp

--- 6.11/1.cpp
+++ 6.11/1.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <conio>
-#include <math>
+#include <math.h>
+using namespace std;
 int main ()
 {
 	int n,P,P1=0,z=1;
-  cout<<"n="; cin>>n;
+	cout<<"n="; cin>>n;
 	while (n!=0)
-   {
-   	  P=n%2;
-      n=n/2;
-      P1=P1+(P*z);
-      z=10*z;
+   	{
+   	 	P=n%2;
+      		n=n/2;
+      		P1=P1+(P*z);
+      		z=10*z;
 	}
-  cout<<P1;
-  getch ();
-  return 0;
+  	cout<<P1;
+  	return 0;
 }
